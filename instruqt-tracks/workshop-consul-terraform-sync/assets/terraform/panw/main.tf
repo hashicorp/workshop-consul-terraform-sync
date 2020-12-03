560febdbee849755e9c7f13c2095398316ef0b04
Nathan Pearce <nathan.pearce@gmail.com>
2020-12-02 18:23:51 -0800

    Add PANW bootstrap setup

--- instruqt-tracks/workshop-consul-terraform-sync/assets/terraform/panw/main.tf
+++ instruqt-tracks/workshop-consul-terraform-sync/assets/terraform/panw/main.tf
@@ -27,6 +27,39 @@ resource "azurerm_storage_account" "PAN_FW_STG_AC" {
   account_tier             = "Standard" 
 }
 
+## START Firewall VM-Series Bootstrap ##
+resource "azurerm_storage_share" "bootstrap-storage-share" {
+  name                 = "bootstrapshare-${var.StorageAccountName}"
+  storage_account_name = azurerm_storage_account.PAN_FW_STG_AC.name
+}
+
+data "template_file" "render-init-cfg" {
+  template = file("${path.module}/init-cfg.tmpl")
+  vars = {
+    "hostname"         = var.hostname,
+    "tplname"          = var.tplname,
+    "dgname"           = var.dgname,
+    "dns-primary"      = var.dns-primary,
+    "dns-secondary"    = var.dns-secondary,
+    "vm-auth-key"      = var.vm-auth-key,
+    "op-command-modes" = var.op-command-modes
+  }
+}
+
+resource "local_file" "write-init-cfg" {
+  content  = data.template_file.render-init-cfg.rendered
+  filename = "${path.root}/files/config/init-cfg.txt"
+}
+
+resource "null_resource" "file_uploads" {
+
+  provisioner "local-exec" {
+    command = "cd ${path.root}/files; az storage file upload-batch --account-name ${azurerm_storage_account.PAN_FW_STG_AC.name} --account-key ${azurerm_storage_account.PAN_FW_STG_AC.primary_access_key} --source . --destination ${azurerm_storage_share.bootstrap-storage-share.name}"
+  }
+}
+## END Firewall VM-Series Bootstrap ##
+
+## START Firewall VM-Series ##
 resource "azurerm_public_ip" "PublicIP_0" {
   name                = var.fwpublicIPName
   location            = data.terraform_remote_state.vnet.outputs.resource_group_location
@@ -147,3 +180,5 @@ resource "azurerm_virtual_machine" "PAN_FW_FW" {
     disable_password_authentication = false
   }
 }
+
+## END Firewall VM-Series ##
--- instruqt-tracks/workshop-consul-terraform-sync/assets/terraform/panw/variables.tf
+++ instruqt-tracks/workshop-consul-terraform-sync/assets/terraform/panw/variables.tf
@@ -10,6 +10,62 @@ variable "FromGatewayLogin" {}
 variable "IPAddressDmzNetwork" {}
 variable "IPAddressAppNetwork" {}
 
+##Bootstrap
+variable "hostname" {
+  default     = ""
+  description = "The hostname of the VM-series instance"
+  type        = string
+}
+
+variable "panorama-server" {
+  default     = ""
+  description = "The FQDN or IP address of the primary Panorama server"
+  type        = string
+}
+
+variable "panorama-server2" {
+  default     = ""
+  description = "The FQDN or IP address of the secondary Panorama server"
+  type        = string
+}
+
+variable "tplname" {
+  default     = ""
+  description = "The Panorama template stack name"
+  type        = string
+}
+
+variable "dgname" {
+  default     = ""
+  description = "The Panorama device group name"
+  type        = string
+}
+
+variable "dns-primary" {
+  default     = ""
+  description = "The IP address of the primary DNS server"
+  type        = string
+}
+
+variable "dns-secondary" {
+  default     = ""
+  description = "The IP address of the secondary DNS server"
+  type        = string
+}
+
+variable "vm-auth-key" {
+  default     = ""
+  description = "Virtual machine authentication key"
+  type        = string
+}
+
+variable "op-command-modes" {
+  default     = ""
+  description = "Set jumbo-frame and/or mgmt-interface-swap"
+  type        = string
+}
+##Bootstrap
+
 variable "storageAccountType" {
   default = "Standard_LRS"
 }
